e75da27cef16b95d5086583afcd781d2ee9224ae
lichuan89 <33598647+lichuan89@users.noreply.github.com>
2018-05-01 22:48:35 +0800

    Update heap_sort.py

--- heap_sort.py
+++ heap_sort.py
@@ -9,35 +9,33 @@
 
 
 def adjust_heap(arr, i, num):
-    """
+    """ 
     已知arr数组表示堆,
     arr[i + 1: num]里的子树满足最大堆，调整arr[i: ]成为最大堆
     """
     # 将根节点的值冒泡下沉到合适的位置
     while True:
         # 取最大子节点
-        left = 2 * i + 1 
-        right = 2 * i + 2 
-        idx = None
+        left, right = 2 * i + 1, 2 * i + 2 
+        select = None
         if left < num:
-            idx = left
-        if right < num and (idx is None or arr[right] > arr[left]):
-            idx = right
-        # 没有子节点, 结束
-        if idx is None:
+            select = left
+        if right < num and (select is None or arr[right] > arr[left]):
+            select = right
+        # 没有子节点或根节点比子节点大, 结束
+        if select is None or arr[i] >= arr[select]:
             break
-        # 根节点比子节点小，交换; 否则结束
-        if arr[i] < arr[idx]:
-            arr[i], arr[idx] = arr[idx], arr[i]
-        i = idx  
-      
-    
+        # 根节点冒泡下沉
+        arr[i], arr[select] = arr[select], arr[i]
+        i = select
+        
+        
 def heap_sort(arr):
     """ 
     堆排序
     """
     # 调整数组成最大堆
-    for i in range(len(arr) / 2 - 1, -1, -1): 
+    for i in range(len(arr) / 2 - 1, -1, -1):
         adjust_heap(arr, i, len(arr))
     # 选取最大值，重新调整最大堆
     for i in range(len(arr) - 1, 0, -1):
@@ -48,4 +46,3 @@ def heap_sort(arr):
 
 if __name__ == "__main__":
     arr = [2, 5, 7, 1, 0, 5]
-    print heap_sort(arr)
